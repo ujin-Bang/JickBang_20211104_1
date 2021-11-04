1514d32ed72d3ec8ee6ae89a3bde8eae1a44a18f
ujin-Bang <qkddnwls14@gmail.com>
2021-11-04 21:46:23 +0900

    부모 생성자의 요구사항 해결

--- app/src/main/java/org/techtown/jickbang_20211104_1/adapters/RoomAdapter.kt
+++ app/src/main/java/org/techtown/jickbang_20211104_1/adapters/RoomAdapter.kt
@@ -7,5 +7,5 @@ import org.techtown.jickbang_20211104_1.datas.RoomData
 class RoomAdapter(
     val mContext: Context,
     val resId: Int,
-    val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>() {
+    val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>( mContext,resId,mList ) {
 }
