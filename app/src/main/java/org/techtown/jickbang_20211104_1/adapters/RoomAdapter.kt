dae31be13a904cf65d5862f1b843045276e71a40
ujin-Bang <qkddnwls14@gmail.com>
2021-11-04 21:42:24 +0900

    RoomAdapter 추가 + ArrayAdapter 상송   - 기본 생성자 지원 X, 추가 작업 필요

---
+++ app/src/main/java/org/techtown/jickbang_20211104_1/adapters/RoomAdapter.kt
@@ -0,0 +1,7 @@
+package org.techtown.jickbang_20211104_1.adapters
+
+import android.widget.ArrayAdapter
+import org.techtown.jickbang_20211104_1.datas.RoomData
+
+class RoomAdapter : ArrayAdapter<RoomData>() {
+}
